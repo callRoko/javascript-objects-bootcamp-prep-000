035cf9c4cc41321d5b32ce0e9a1390daae7ca17f
Hazel Donaldson <hazel90.hd@gmail.com>
2019-04-04 15:23:29 +0000

    Automatically backed up by Learn

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = {
+  "Michael Jackson": "Human Nature",
+  "Kendrick Lamar": "Sing About Me",
+  "Aaliyah":"We Need A Resolution",
+}
+
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  Object.assign({}, "artistName: songTitle")
+  return playlist
+}
